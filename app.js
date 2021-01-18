a2634d8ba01a8fad3fba2bcc2def2de161935e3e
mohamed <mohamed.abdenebi@outlook.com>
2021-01-18 16:47:21 +0100

    addCreateNewlineName

--- app.js
+++ app.js
@@ -9,4 +9,25 @@ function addNameTab() {
     console.log(newName);
     listName.push(newName);
     console.log(listName);
+
+    //rajouter une ligne
+    addLine(newName);
 }
+//add line #id newLine
+function addLine(newName){
+    const myLine = document.querySelector("#newLine")
+    const myLi = document.createElement("li") //create a li
+    myLi.innerText = newName; // add name in li
+     myLine.appendChild(myLi) // paste child to parent
+    //editButton(myLi)
+}
+/*// create div edit and delete
+function editButton(element){
+    const myLine = document.querySelector(element);
+   //div
+    const myDiv = document.createElement("div");
+    //button
+    const myButtonDelete = document.createElement("button")
+    //button
+    myLine.appendChild(myDiv,myButtonDelete)
+}*/
